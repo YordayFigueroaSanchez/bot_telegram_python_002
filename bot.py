1586ecef0ff66a08bbbc4ab2c9d7ba8cb1081358
Yorday Figueroa <yfsanchez@gmail.com>
2021-04-18 18:13:12 -0300

    Update bot.py
    fin de video 1

--- bot.py
+++ bot.py
@@ -1,7 +1,8 @@
-from telegram.ext import Updater, CommandHandler
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 import logging
 import os
 import telegram
+import random
 
 #Configure logging
 logging.basicConfig(
@@ -14,7 +15,25 @@ TOKEN = os.getenv("TOKEN")
 print(TOKEN)
 
 def start(update, context):
-    print(update)
+    #print(update)
+    logger.info(f"El usuario {update.effective_user['id']}, ha iniciado una convesacion")
+    name = update.effective_user['first_name']
+    update.message.reply_text(f"Hola {name} soy tu bot")
+
+def random_number(update, context):
+    user_id = update.effective_user['id']
+    number = random.randint(0,10)
+    logger.info(f"El usuario {user_id}, ha solicitado numero aleatorio : {number}")
+    context.bot.sendMessage(chat_id=user_id, parse_mode="HTML",text=f"<b>Numero</b> aleatorio: \n{number}")
+
+def echo(update, context):
+    user_id = update.effective_user['id']
+    logger.info(f"El usuario {user_id}, escribio un mensaje de texto")
+    text = update.message.text
+    context.bot.sendMessage(
+        chat_id=user_id, 
+        parse_mode="MarkdownV2",
+        text=f"*Escibiste:* \n _{text}_")
 
 if __name__ == "__main__":
     #Obtener informacion de nuestro bot
@@ -29,9 +48,12 @@ dp = updater.dispatcher
 
 #crear manejadores
 dp.add_handler(CommandHandler("start", start))
+dp.add_handler(CommandHandler("random", random_number))
+dp.add_handler(MessageHandler(Filters.text, echo))
 
 #
 updater.start_polling()
+print("BOT CARGADO")
 
 #permite finalizar con Ctrl+C
 updater.idle()
